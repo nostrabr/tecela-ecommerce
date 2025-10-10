cad26a38daf562b20e2af0b000d707a6dc88c590
Weslei Casali <103540440+CasaliWe@users.noreply.github.com>
2025-10-10 13:02:13 -0300

    att

--- bd/conecta.php
+++ bd/conecta.php
@@ -3,10 +3,10 @@
 //CONECTA COM O BANCO DE DADOS
 
 //SETA OS DADOS DO SERVIDOR
-$servername = '162.241.129.229';
-$database = 'tecela_banco';
-$username = 'tecela_admin';
-$password = 'w1e2s3l4e5i6';
+$servername = '31.97.12.109';
+$database = 'tebanco';
+$username = 'teuser';
+$password = '@Mvp258080nostra';
 
 //CRIA A CONEXÃO
 $conn = mysqli_connect($servername, $username, $password, $database);
